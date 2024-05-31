6b750205fe991d9f60d81a7c4df04d2d4d098a45
mytho <155643316+qvnsecret@users.noreply.github.com>
2024-05-31 19:37:00 +0200

    Update JoinNow.js

--- src/components/JoinNow.js
+++ src/components/JoinNow.js
@@ -4,14 +4,14 @@ const JoinNow = () => {
   return (
     <>
       <section className="joinUs">
-        <div className="h-color joinUs-header">
+        <div className="h-color joinUs-header" href="https://discord.gg/uzsBkB9wm4">
           <h3>Join us now!</h3>
         </div>
         <div className="joinUs-para p-color">
           Invite our bot to your Discord server!{" "}
         </div>
         <button className="btn">
-          <Link className="h-color no-decoration" href="#">
+          <Link className="h-color no-decoration" href="https://discord.com/oauth2/authorize?client_id=1223343717184835754">
             Invite now!
           </Link>
         </button>

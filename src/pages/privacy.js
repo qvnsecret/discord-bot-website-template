c36f5df709966558ddc26492a8b32d620cb8285e
mytho <155643316+qvnsecret@users.noreply.github.com>
2024-05-31 22:17:51 +0200

    Update privacy.js

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -10,10 +10,10 @@ export default function Commands() {
       animate={{ opacity: 1 }}
     >
       <Head>
-        <title>Commands | ONbot</title>
+        <title>Privacy | ONbot</title>
         <meta
           name="description"
-          content="Commands page | Minimal and awesome discord bot website created with ❤️ using NextJS"
+          content="Privacy | Minimal and awesome discord bot website created with ❤️ using NextJS"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />

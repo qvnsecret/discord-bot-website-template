e98e29392976737fee75e840a766571f389d9ff9
mytho <155643316+qvnsecret@users.noreply.github.com>
2024-06-01 18:47:59 +0200

    Update _test.js

--- src/pages/_test.js
+++ src/pages/_test.js
@@ -22,7 +22,7 @@ export default function Commands() {
       <section className="about-section">
         <nav className="accordion arrows" id="commands">
           <main className="showcase">
-            <h1 className="showcase-header">ONbot</h1>
+            <h1 className="showcase-header h-color">ONbot</h1>
             <h2 className="showcase-header-text h-color">
               The only discord bot you need!
             </h2>
@@ -31,24 +31,7 @@ export default function Commands() {
               your needs in your discord server.
             </p>
           </main>
-
-          <input type="radio" name="accordion" id="cb3" />
-          <section className="box">
-            <label className="box-title" htmlFor="cb3">
-              📘Privacy Statement
-            </label>
-            <label className="box-close" htmlFor="acc-close"></label>
-            <div className="box-content">
-              <ul>
-                <li>
-                  <kbd></kbd> -{" "}
-                  <span>
-                    At Mytho, operated by Reduce, we hold the utmost respect for your privacy. Only the Founder & CEO of the project has access to your personal information. Your privacy is of paramount importance to us. Please be assured that any personal information we collect is solely for educational purposes.
-                    This statement makes clear who has access to personal information and the purpose behind collecting it, maintaining transparency and building trust.
-                  </span>
-                </li>
-              </ul>
-            </div>
+       </div>
           </section>
         </nav>
       </section>

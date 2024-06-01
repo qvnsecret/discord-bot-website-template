a4851a9f9e6ad15528ce3e522f529524095c9088
mytho <155643316+qvnsecret@users.noreply.github.com>
2024-06-01 09:22:55 +0200

    Update Showcase.js

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -10,7 +10,7 @@ const Showcase = () => {
       animate={{ opacity: 1 }}
     >
       <main className="showcase">
-        <h1 className="showcase-header h-color">ONbot</h1>
+        <h1 className="showcase-header h-animated">ONbot</h1>
         <h2 className="showcase-header-text h-color">
           The only discord bot you need!
         </h2>

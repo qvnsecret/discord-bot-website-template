9d568bf5658f23eb7a16f3ad8efc697f9c8127e0
mytho <155643316+qvnsecret@users.noreply.github.com>
2024-06-01 21:21:40 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,13 @@ import "@/styles/partials/_showcase.scss";
 import "@/styles/partials/_about.scss";
 import "@/styles/partials/_joinUs.scss";
 import "@/styles/partials/_footer.scss";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Analytics />
+      <Component {...pageProps} />
+    </>
+  );
 }

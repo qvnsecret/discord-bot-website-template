e9c2a1bb9e0687822050b6282851b198019a0fb7
mytho <155643316+qvnsecret@users.noreply.github.com>
2024-06-24 23:06:59 +0200

    Update hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'Administration ONbot' })
+  res.status(200).json({ name: 'The page is operating exceptionally well. For any issues, please contact us at https://discord.gg/GJBkaHxfEN.' })
 }
